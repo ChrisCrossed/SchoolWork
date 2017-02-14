424e55153b4b7915c8210d988b3bd66557b7f9c0
Chris Christensen <ChrisCrossed.86@gmail.com>
2017-02-13 20:51:46 -0800

    Working on Buttons & 'Use' Items

---
+++ Personal Projects/FPSProject/Assets/Cs_UseButton.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cs_UseButton : MonoBehaviour
+{
+    // Object Connection
+    [SerializeField] GameObject ConnectedObject;
+    [SerializeField] bool UseOnce;
+
+	// Use this for initialization
+	void Start ()
+    {
+		if(ConnectedObject == new GameObject() || ConnectedObject == null )
+        {
+            print(gameObject.name + " is missing a Connected Object!");
+        }
+	}
+
+    bool b_Active;
+    public bool ButtonState
+    {
+        get { return b_Active; }
+        set
+        {
+            if(value == true || !UseOnce )
+            {
+                b_Active = value;
+            }
+        }
+    }
+
+    public void Use_Button()
+    {
+        ButtonState = !ButtonState;
+    }
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+
+		if(b_Active)
+        {
+
+        }
+        else
+        {
+
+        }
+	}
+}
