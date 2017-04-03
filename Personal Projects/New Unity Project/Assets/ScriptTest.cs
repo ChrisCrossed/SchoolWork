55773be128a226f9bc83c48b0912fe98688cafff
Chris Christensen <ChrisCrossed.86@gmail.com>
2017-04-03 16:35:39 -0700

    Updates to Unity3d (5.6.0f3) and ProBuilder.

---
+++ Personal Projects/New Unity Project/Assets/ScriptTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    int i_ = 0;
+    bool RecursiveNumber()
+    {
+        print(i_);
+
+        ++i_;
+
+        if (i_ >= 100) return true;
+        else
+        {
+            RecursiveNumber();
+        }
+
+        return false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(RecursiveNumber())
+        {
+            print("Ended");
+        }
+	}
+}
