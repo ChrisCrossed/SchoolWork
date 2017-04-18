8d3b08120f4df58239cc77c38b03b15afebd177e
Chris Christensen <ChrisCrossed.86@gmail.com>
2017-04-18 13:58:18 -0700

    GAT261_Project3 - Creating vehicle

---
+++ School/GAT261_Project3/Assets/Cs_VehicleLogic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cs_VehicleLogic : MonoBehaviour
+{
+    [SerializeField] GameObject go_LookAtBox;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
