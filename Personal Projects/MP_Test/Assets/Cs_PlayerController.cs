5b4c9e8bfc51cdf3242a345dce8ae594ec5544b1
Chris Christensen <C.Christensen@DigiPen.edu>
2017-05-07 19:56:45 -0700

    New Project - MP Test

---
+++ Personal Projects/MP_Test/Assets/Cs_PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Cs_PlayerController : NetworkBehaviour
+{
+    public override void OnStartLocalPlayer()
+    {
+        base.OnStartLocalPlayer();
+
+        gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (!isLocalPlayer) return;
+
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150f;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        gameObject.transform.Rotate(0, x, 0);
+        gameObject.transform.Translate(0, 0, z);
+	}
+}
