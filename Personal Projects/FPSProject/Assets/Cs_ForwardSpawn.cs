a05e520ef27db2e168879de24b22865362713afb
Chris Christensen <C.Christensen@DigiPen.edu>
2017-07-02 14:45:39 -0700

    GAM 450 - Maelstrom
    - 1-1c

---
+++ Personal Projects/FPSProject/Assets/Cs_ForwardSpawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cs_ForwardSpawn : MonoBehaviour
+{
+    [SerializeField] bool Active = false;
+
+    // Player Connection
+    GameObject go_Player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        gameObject.GetComponent<Collider>().enabled = false;
+
+		if(Active)
+        {
+            go_Player = GameObject.Find("Player");
+
+            go_Player.transform.position = gameObject.transform.position;
+            go_Player.transform.rotation = gameObject.transform.rotation;
+        }
+
+        new WaitForEndOfFrame();
+
+        GameObject.Destroy(gameObject);
+	}
+}
